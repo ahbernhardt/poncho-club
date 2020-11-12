91649fac60419d062d232b26f43c3405e439d082
dylanbernhardt <30446845+dylanbernhardt@users.noreply.github.com>
2020-11-12 11:16:59 -0600

    Location (#18)
    * update menu
    
    * update menu
    
    * fix update menu
    
    * add prices
    
    * menu formatting
    
    * update twitch
    
    * update big pizza
    
    * fix location

--- src/components/HomePage/Intro/index.js
+++ src/components/HomePage/Intro/index.js
@@ -4,9 +4,11 @@ import { Wrapper, Title } from './style'
 const Intro = props =>
     <Wrapper {...props} >
         <Title>
-            <span className="name">PONCHO (Ward 4 - Plot 55 - MISTS - EXODUS)</span>
+            <span className="name">PONCHO</span>
         </Title>
         <Title>
+            <span className="day">Ward 4 - Plot 55 - MISTS - EXODUS</span>
+            <br/>
             <span className="day">Saturdays</span>
             <br/>
             <span className="time">10pm - Close (CST)</span>
