53888308daff3f45ebd3766e49ed1caa08dbed06
isaro <wavealden@gmail.com>
2020-11-12 10:29:14 -0600

    typo

--- src/components/AboutPage/AboutContent/index.js
+++ src/components/AboutPage/AboutContent/index.js
@@ -50,11 +50,12 @@ export default class extends Component {
                     image={META.common.image}
                 />
 
-                <AboutIntroSection text="PONCHO (Ward 4 - Plot 55 - MISTS - EXODUS)"
+                <AboutIntroSection text="PONCHO"
                     content={() =>
                         <>
-                        The club was constructed on top of the remains of a burned down factory. The factory itself was ironically burned down due to a massive rave that was thrown. Poncho (Now adhering to fire codes) stands as an epicenter of EDM and partying in Eorzea.
-                        <br/>Poncho is a specialized Trance music venue that offers a sanctuary for partygoers and music lovers alike.</main>
+                            The club was constructed on top of the remains of a burned down factory. The factory itself was ironically burned down due to a massive rave that was thrown. Poncho (Now adhering to fire codes) stands as an epicenter of EDM and partying in Eorzea.
+                            <br />
+                            Poncho is a specialized Trance music venue that offers a sanctuary for partygoers and music lovers alike.
                         </>
                     }
                 />
