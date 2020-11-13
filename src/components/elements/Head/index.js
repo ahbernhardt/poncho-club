04d092ee959fe185326f554e02fb476f66809e3d
isaro <wavealden@gmail.com>
2020-11-13 16:39:13 -0600

    'header'

--- src/components/elements/Head/index.js
+++ src/components/elements/Head/index.js
@@ -8,11 +8,7 @@ const Head = ({ title, description, image }) =>
         <meta name="description" content={description} />
         <meta property="og:title" content={title}/>
         <meta property="og:description" content={description}/>
-<<<<<<< HEAD
         <meta property="og:image" content="/images/poncho.png" />
-=======
-        <meta property="og:image" content="../../../assets/favicon.png" />
->>>>>>> 054a627a96a4eeb7976c3056539307a82a32ad90
         {/*<meta name="google-site-verification" content="w6QODQIo6PYp7OLUH3RI1ULc4NKzA0lt7tzXEeKJzjM" />*/}
         <link rel="stylesheet"
                   href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
